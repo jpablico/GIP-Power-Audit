338f6323a893adf1a6f9580d8bed0dce366a3bcc
Josh Pablico <jbpablico27@gmail.com>
2025-06-18 02:07:55 -0500

    I can't spell

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import DataCenterPowerAudit from './scripts/DataCenterPowerAudit';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <header className="bg-oragne-500 text-white shadow">
+      <header className="bg-orange-500 text-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold">Global IP Power Audit Tool</h1>
         </div>
